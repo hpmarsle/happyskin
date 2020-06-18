e383d8fa8c07a019e9fc3190ca36064c1fa4f6fc
Maria Le <hp.mars.le@gmail.com>
2020-06-18 16:55:18 -0500

    imported composeWithDevTools to have thunk work with devtools

--- src/store.js
+++ src/store.js
@@ -1,15 +1,17 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import thunk  from 'redux-thunk';
 import productsReducer from './reducers/products'
+import { composeWithDevTools } from 'redux-devtools-extension'
+
 const reducer = combineReducers({
     productsReducer//,
     //reviewsReducer
   })
 
+// const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; 
+
 const store = createStore(
-    reducer,
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    reducer, composeWithDevTools(applyMiddleware(thunk))
 )
 
 export default store;
