ebbb4f013b8bde03cea26add142be6e0dd5b61ae
Maria Le <hp.mars.le@gmail.com>
2020-06-18 16:59:58 -0500

    removed comment

--- src/store.js
+++ src/store.js
@@ -8,8 +8,6 @@ const reducer = combineReducers({
     //reviewsReducer
   })
 
-// const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; 
-
 const store = createStore(
     reducer, composeWithDevTools(applyMiddleware(thunk))
 )
