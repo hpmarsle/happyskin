ed4ba2180d51b57a641f3b202785f5d69f1768fe
Maria Le <hp.mars.le@gmail.com>
2020-06-18 13:41:14 -0500

    import redux packages

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import { thunk } from 'redux-thunk';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
   (<Router>
