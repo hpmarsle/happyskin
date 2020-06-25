281d87452dddfdb2b91263d9f568255fbafc19b4
Maria Le <hp.mars.le@gmail.com>
2020-06-25 00:40:25 -0500

    moved async action creator fetch request to ProductsContainer

--- src/App.js
+++ src/App.js
@@ -2,28 +2,17 @@ import React from 'react';
 import ProductsContainer from './containers/ProductsContainer';
 import { Route } from 'react-router-dom'
 import NavBar from './Navbar';
-import { fetchProducts } from './actions/products'
-import { connect } from 'react-redux'
 
 class App extends React.Component {
-
-  componentDidMount(){
-    this.props.fetchProducts()
-  }
 
   render() {
     return (
     <div className="App">
       <NavBar />
-      
      <ProductsContainer />
     </div>
     );
   }
 }
 
-const mapStateToProps = state => {
-  return {products: state.products, loading: state.loading}
-}
-
-export default connect(mapStateToProps, {fetchProducts})(App);
+export default App;
--- src/containers/ProductsContainer.js
+++ src/containers/ProductsContainer.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react'
+import { fetchProducts } from '../actions/products'
+import { connect } from 'react-redux'
+import Products from '../components/Products'
 
-export default class ProductsContainer extends Component {
+class ProductsContainer extends Component {
+    componentDidMount() {
+        this.props.fetchProducts()
+    }
+
     render() {
+        
         return (
             <div>
                 <h1>All of my products will display on the home page</h1>
+                
+                <Products products={this.props.products} />
             </div>
         )
     }
+    
 }
+const mapStateToProps = state => {
+    return {
+        products: state.products 
+    }
+}
+  
+export default connect(mapStateToProps, {fetchProducts})(ProductsContainer);
