7c5133d81213253bdd0dc18238f9482614515c82
Maria Le <hp.mars.le@gmail.com>
2020-06-25 23:01:06 -0500

    map over states products to make ProductCard child for each product object

--- src/components/Products.js
+++ src/components/Products.js
@@ -6,8 +6,20 @@ export default function Products(props) {
     return (
         <div>
             Products here
-            <ProductCard />
-            {/*props.products.map(product => <li key={product.id}>{product.name}</li>)*/}
+            {props.products.map(product => 
+                < ProductCard 
+                key={product.id} 
+                name={product.name} 
+                brand={product.brand} 
+                image={product.image} 
+                ingredients={product.ingredients} 
+                description={product.description}
+                size={product.size}
+                price={product.price}
+                qty={product.quantity}
+                reviews={product.reviews[0].star_rating}
+                /> 
+            )}
         </div>
     )
 }
