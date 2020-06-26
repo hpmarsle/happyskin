b9fe0c31ce9971a59bcc89d5aba24761f1276113
Maria Le <hp.mars.le@gmail.com>
2020-06-25 23:00:07 -0500

    construct test JSX for ProductCard to get passed props

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,7 +1,20 @@
 import React from 'react'
 
 const ProductCard = (props) => {
-    return (<div>SINGLE PRODUCT RENDERED</div>)
+    return (
+        <div id={props.key}>  
+            <p>{props.name} </p>
+            <p>{props.brand} </p>
+            <p>{props.image} </p>
+            <p>{props.ingredients} </p>
+            <p>{props.description} </p>
+            <p>{props.size} </p>
+            <p>{props.quantity} </p>
+            <p>{props.reviews} </p>
+            SINGLE PRODUCT RENDERED
+
+        </div>
+    )
 }
 
 export default ProductCard
