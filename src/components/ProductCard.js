fdbcb74d796f63bac254467904d151c35b2c4a9e
Maria Le <hp.mars.le@gmail.com>
2020-06-26 20:31:01 -0500

    start styling product card, align div center, include styled compnent for product image

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,25 +1,36 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const Paragraph = styled.p`
-    background: pink;
-    font-size: 2em;
+const StyledDiv = styled.div`
+    margin: 20px;
+    padding: 10px;
+    border: solid pink;
+    text-align: center;
+
+`
+const Image = styled.img`
+    border: 1px solid #ddd;
+    border-radius: 4px;
+    padding: 5px;
+    width: 150px;
 `
 
 const ProductCard = (props) => {
     return (
-        <div id={props.key} >  
-            <Paragraph>{props.name} </Paragraph>
-            <p>{props.brand} </p>
-            <p>{props.image} </p>
-            <p>{props.ingredients} </p>
-            <p>{props.description} </p>
-            <p>{props.size} </p>
-            <p>{props.quantity} </p>
-            <p>{props.reviews} </p>
-            SINGLE PRODUCT RENDERED
+        <StyledDiv>
+            <div id={props.key} >  
+                <Image src={props.image} alt="product pic"/>
+                <div> {props.name} </div>
+                <p>{props.brand} </p>
+                <p>{props.ingredients} </p>
+                <p>{props.description} </p>
+                <p>{props.size} </p>
+                <p>{props.quantity} </p>
+                <p>{props.reviews} </p>
+                SINGLE PRODUCT RENDERED
 
-        </div>
+            </div>
+        </StyledDiv>
     )
 }
 
