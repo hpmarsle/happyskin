5e42dca783ac5a6df3b3938da4591bf452731a19
Maria Le <hp.mars.le@gmail.com>
2020-06-26 20:33:42 -0500

    condense productCard to include only name, brand, price, and review star rating

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -21,13 +21,9 @@ const ProductCard = (props) => {
             <div id={props.key} >  
                 <Image src={props.image} alt="product pic"/>
                 <div> {props.name} </div>
-                <p>{props.brand} </p>
-                <p>{props.ingredients} </p>
-                <p>{props.description} </p>
-                <p>{props.size} </p>
-                <p>{props.quantity} </p>
-                <p>{props.reviews} </p>
-                SINGLE PRODUCT RENDERED
+                <div>{props.brand} </div>
+                <div>${props.price} </div>
+                <div>{props.reviews} </div>
 
             </div>
         </StyledDiv>
