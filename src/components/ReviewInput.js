b51deeb280f99162302813dd9f6f697b86e13d75
Maria Le <hp.mars.le@gmail.com>
2020-06-30 00:20:44 -0500

    boilerplate for reviews child components in reviewscontainer parent component

---
+++ src/components/ReviewInput.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+export default class ReviewInput extends React.Component {
+    render() {
+        return (
+            <div>
+                Product Review Form goes here.
+            </div>
+        )
+    }
+}
--- src/containers/ReviewsContainer.js
+++ src/containers/ReviewsContainer.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
+import ReviewInput from '../components/ReviewInput'
+import Reviews from '../components/Reviews'
 
 export default class ReviewsContainer extends Component {
     render() {
         return (
             <div>
                 Reviews Container
+                <ReviewInput />
+                <Reviews />
             </div>
         )
     }
