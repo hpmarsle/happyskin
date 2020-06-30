139dc3039de5b1736de63f17fd4b3900d780a856
Maria Le <hp.mars.le@gmail.com>
2020-06-29 23:41:54 -0500

    render placeholder text for individual product info

--- src/components/ProductPage.js
+++ src/components/ProductPage.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import ProductCard from './ProductCard'
 
 const ProductPage = (props) => {
 
@@ -9,6 +10,12 @@ const ProductPage = (props) => {
     return (
         <div>
             <h1>{product ? product.name : null}</h1>
+            <h3>{product ? product.brand : null}</h3>
+            <h3>{product ? product.price : null}</h3>
+            <h3>{product ? product.reviews[0].star_rating : null}</h3>
+            <p>{product ? product.ingredients : null}</p>
+            {product ? <img src={product.image} alt="product"/> : null}
+   
         </div>
     )
 }
