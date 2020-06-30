805fab20d83786d395208b1b3178b5a2be19e856
Maria Le <hp.mars.le@gmail.com>
2020-06-29 23:39:26 -0500

    create link for each product card, when clicked it will take you to the product show page

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ProductCard from './ProductCard'
+import { Link } from 'react-router-dom'
 
 export default function Products(props) {
 
@@ -7,14 +8,16 @@ export default function Products(props) {
         <div>
             Products here
             {props.products.map(product => 
+                <Link to ={`/products/${product.id}`}>
                 < ProductCard 
-                key={product.id} 
+                id={product.id} 
                 name={product.name} 
                 brand={product.brand} 
                 image={product.image} 
                 price={product.price}
                 reviews={product.reviews[0].star_rating}
                 /> 
+                </Link>
             )}
         </div>
     )
