fe4bf3af39803905f3b9713c2fe7f3fc493dc991
Maria Le <hp.mars.le@gmail.com>
2020-07-01 16:16:50 -0500

    test Reviews are mounting in the ReviewsContainer

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -1,9 +1,21 @@
 import React from 'react'
 
-export default function Reviews() {
+export default function Reviews(props) {
     return (
         <div>
             Reviews are rendering in the container.
+            {props.reviews.map(review => 
+                <div key={review.id}>
+                    <div>
+                        <div>{review.star_rating} </div>
+                    </div>
+
+                    <div>
+                        <div>{review.comments} </div>
+                    </div>
+                </div>
+            )}
         </div>
     )
 }
+ 
