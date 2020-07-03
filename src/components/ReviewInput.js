c423b577c1ecc8b2b986166e991ce2c27c70ffe9
Maria Le <hp.mars.le@gmail.com>
2020-07-02 23:34:51 -0500

    set initial local state of form and input values according to state keys

--- src/components/ReviewInput.js
+++ src/components/ReviewInput.js
@@ -2,14 +2,19 @@ import React from 'react'
 
 
 export default class ReviewInput extends React.Component {
+    state = {
+        star_rating: '',
+        comments: ''
+    }
+    
     render() {
         return (
             <div>
                 <form>
                     <label>Rating</label>
-                    <input type="text"/>
+                    <input type="text"name="star_rating" value={this.state.star_rating} />
                     <label>Comments:</label>
-                    <input type="textarea"/>
+                    <input type="textarea"name="comments" value={this.state.comments} />
                     <input type="submit"/>
                 </form>
             </div>
