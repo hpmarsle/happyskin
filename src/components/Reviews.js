a0e740894f2a903344e669472ecf6e8c163389f4
Maria Le <hp.mars.le@gmail.com>
2020-07-08 03:01:26 -0500

    add condition to render No Reviews string if the product does not have any reviews yet

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -6,23 +6,28 @@ function Reviews(props) {
     const handleDelete = (reviewId, productId) => {
         props.deleteReview(reviewId, productId)
     }
-    return (
-        <div>
-            Reviews are rendering in the container.
-            {props.reviews && props.reviews.map(review => 
-                <div key={review.id}>
-                    <div>
-                        <div>{review.star_rating} </div>
-                    </div>
+    if (props.reviews  === undefined || props.reviews  == 0) {
+        return (<h3>No Reviews</h3>)
+    } else {
 
-                    <div>
-                        <div>{review.comments} </div>
+        return (
+            <div>
+                Reviews are rendering in the container.
+                {props.reviews && props.reviews.map(review => 
+                    <div key={review.id}>
+                        <div>
+                            <div>{review.star_rating} </div>
+                        </div>
+    
+                        <div>
+                            <div>{review.comments} </div>
+                        </div>
+                        <button onClick={() => handleDelete(review.id, review.product_id)}>Delete</button>
                     </div>
-                    <button onClick={() => handleDelete(review.id, review.product_id)}>Delete</button>
-                </div>
-            )}
-        </div>
-    )
+                )}
+            </div>
+        )
+    }
 }
  
 export default connect(null, {deleteReview})(Reviews)
