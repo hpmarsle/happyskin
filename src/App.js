577c2ade288eb9314cc78f6e7439b4786a85fdde
Maria Le <hp.mars.le@gmail.com>
2020-07-13 22:05:04 -0500

    moved BrowserRouter to App component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import ProductsContainer from './containers/ProductsContainer';
-import { Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import NavBar from './components/navigation/Navbar';
+import About from './components/navigation/About'
+import Login from './components/navigation/Login' 
 
 class App extends React.Component {
 
   render() {
     return (
     <div className="App">
+      <Router>
       <NavBar />
-     <ProductsContainer />
+
+        <Route path="/" component={ProductsContainer}/> 
+        <Route path="/about" component={About}/> 
+        <Route path="/login" component={Login}/> 
+     {/*<ProductsContainer />*/}
+      </Router>
     </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,22 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
-import About from './components/navigation/About'
-import Login from './components/navigation/Login'
 import { Provider } from 'react-redux';
 import store from './store'
 
 ReactDOM.render(
-  (<Router>
-    <div>
-      <Provider store={store}>
-        <Route path="/" component={App}/> 
-        <Route path="/about" component={About}/> 
-        <Route path="/login" component={Login}/> 
-      </Provider>
-    </div>
-  </Router>),
-
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
