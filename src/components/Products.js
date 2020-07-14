dade81d8278839a48b13f11502cd5ab7a911d7dc
Maria Le <hp.mars.le@gmail.com>
2020-07-14 00:44:17 -0500

    pass product id as key prop

--- src/components/Products.js
+++ src/components/Products.js
@@ -7,15 +7,15 @@ export default function Products(props) {
     return (
         <div>
             {props.products.map(product => 
-                <Link to ={`/products/${product.id}`}>
-                < ProductCard 
-                id={product.id} 
-                name={product.name} 
-                brand={product.brand} 
-                image={product.image} 
-                price={product.price}
-                reviews={product.reviews}
-                /> 
+                <Link to ={`/products/${product.id}`} key={product.id}>
+                    <ProductCard 
+                    id={product.id} 
+                    name={product.name} 
+                    brand={product.brand} 
+                    image={product.image} 
+                    price={product.price}
+                    reviews={product.reviews}
+                    /> 
                 </Link>
             )}
         </div>
