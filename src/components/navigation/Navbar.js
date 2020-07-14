ed1d8770433adc7f05426e8665808d1810da7c06
Maria Le <hp.mars.le@gmail.com>
2020-07-13 22:23:17 -0500

    add classNames in order to assign basic styling

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -4,20 +4,23 @@ import { NavLink } from 'react-router-dom';
 class Navbar extends React.Component {
     render() {
       return (
-        <div>
+        <div className='navbar'>
           <NavLink
             to="/"
             exact
+            className='navlink'
           >Home</NavLink>
 
           <NavLink
             to="/about"
             exact
+            className='navlink'
           >About</NavLink>
 
           <NavLink
             to="/login"
             exact
+            className='navlink'
           >Login</NavLink>
         </div>
       )
