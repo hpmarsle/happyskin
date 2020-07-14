2d049631dd7e275b9fdb56a958923211aa038c61
Maria Le <hp.mars.le@gmail.com>
2020-07-13 22:43:07 -0500

    created home component to render productscontainer at root route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ProductsContainer from './containers/ProductsContainer';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import NavBar from './components/navigation/Navbar';
 import About from './components/navigation/About'
 import Login from './components/navigation/Login' 
+import Home from './components/navigation/Home' 
 import './App.css'
 
 class App extends React.Component {
@@ -14,7 +14,7 @@ class App extends React.Component {
       <Router>
       <NavBar />
 
-        <Route path="/" component={ProductsContainer}/> 
+        <Route path="/" component={Home}/> 
         <Route path="/about" component={About}/> 
         <Route path="/login" component={Login}/> 
      {/*<ProductsContainer />*/}
---
+++ src/components/navigation/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ProductsContainer from '../../containers/ProductsContainer'
+
+const Home = () => {
+    return (
+        <div>
+            <ProductsContainer />
+        </div>
+    )
+}
+
+export default Home
