3a85654a3651c4cf6d847ea355d0e923433a7884
Maria Le <hp.mars.le@gmail.com>
2020-07-14 23:18:52 -0500

    create async action to edit review

--- src/actions/reviews.js
+++ src/actions/reviews.js
@@ -25,3 +25,21 @@ export const deleteReview = (reviewId, productId) => {
         .then(product => dispatch({type: 'DELETE_REVIEW', payload: product}))
     }
 }
+
+export const editReview = (reviewId, productId) => {
+
+    return (dispatch) => {
+        fetch(`http://localhost:3000/api/v1/products/${productId}/reviews/${reviewId}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            }, 
+            body: JSON.stringify(review)
+        })
+        .then(response => response.json())
+        .then(product => dispatch({
+            type: 'EDIT_REVIEW', 
+            payload: product
+        }));
+    }
+}
