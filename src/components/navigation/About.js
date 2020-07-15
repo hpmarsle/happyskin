077ce706d00a2cb7edc0aca550d8fc7753df3ea7
Maria Le <hp.mars.le@gmail.com>
2020-07-14 23:43:12 -0500

    render About page mockup

--- src/components/navigation/About.js
+++ src/components/navigation/About.js
@@ -3,7 +3,11 @@ import React from 'react'
 export default function About() {
     return (
         <div>
-            About blurbbbb
+            <h1>About Happy Skin</h1>
+            <p>
+                Happy Skin is a place where you can only review products for now.
+                Future updates, this will be an app that will mock an ecommerce store where you can sign up for an account, login, and buy the products by adding them to a shopping cart.                
+            </p>
         </div>
     )
 }
