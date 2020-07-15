c6c1efa6dd3a498494f76197abdb1632f3476f12
Maria Le <hp.mars.le@gmail.com>
2020-07-14 23:46:48 -0500

    render Login page mockup

--- src/components/navigation/Login.js
+++ src/components/navigation/Login.js
@@ -3,7 +3,13 @@ import React from 'react'
 export default function Login() {
     return (
         <div>
-            This will be Login / Setup Account page feature
+            <h1>Login/SignUp</h1>
+            <h4>This will be Login / Setup Account page feature.</h4>
+            <form>
+                <input type='text' placeholder='username' />
+                <input type='password' placeholder='password' />
+                <input type='submit' placeholder='Login/Signup' />
+            </form>
         </div>
     )
 }
