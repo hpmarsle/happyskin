6314b2804258ab542ce5718a32bc4a0bd29f1082
Maria Le <hp.mars.le@gmail.com>
2020-07-28 09:57:39 -0500

    changed to class component, used Link from react-router to include location state

--- src/components/Review.js
+++ src/components/Review.js
@@ -1,23 +1,35 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { deleteReview } from '../actions/reviews'
+import { Link } from 'react-router-dom'
 
-function Review({id, star_rating, comments, productId, deleteReview}) {
 
-    return (
-        <div id={id}>
-            <div>
-                <div className='star_rating'>{star_rating} </div>
+class Review extends React.Component {
+
+    render() {
+        const {id, star_rating, comments, productId, deleteReview} = this.props
+
+        return (           
+            <div id={id}>
+                <div>
+                    <div className='star_rating'>{star_rating} </div>
+                </div>
+    
+                <div>
+                    <div className='comments'>{comments} </div>
+                </div>
+                {/*<button onClick={event =>  window.location.href=`/products/${productId}/reviews/${id}`}>Edit</button>*/}
+                <Link to={{
+                    pathname: `/products/${productId}/reviews/${id}`,
+                    state: { star_rating, comments }
+                }}>
+                    Edit
+                </Link>
+                <button onClick={() => deleteReview(id, productId)}>Delete</button>
+                <br /><br />
             </div>
-
-            <div>
-                <div className='comments'>{comments} </div>
-            </div>
-            <button onClick={event =>  window.location.href=`/products/${productId}/reviews/${id}`}>Edit</button>
-            <button onClick={() => deleteReview(id, productId)}>Delete</button>
-            <br /><br />
-        </div>
-    )
+        )
+    }
 }
 
 export default connect(null, {deleteReview})(Review)
