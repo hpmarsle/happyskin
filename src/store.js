be4e2a413d3cebb5c72788bec1c14ffa6e573737
Maria Le <hp.mars.le@gmail.com>
2020-09-23 13:11:48 -0500

    set up users and currentUser reducer

--- src/store.js
+++ src/store.js
@@ -1,10 +1,14 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import thunk  from 'redux-thunk';
+import users from './reducers/users'
+import currentUser from './reducers/currentUser'
 import products from './reducers/products'
 import reviews from './reducers/reviews'
 import { composeWithDevTools } from 'redux-devtools-extension'
 
 const reducer = combineReducers({
+  users,
+  currentUser,
   products,
   reviews
 })
