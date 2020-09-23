19ff661c4b8350cf3fec55e91c981329eae138f2
Maria Le <hp.mars.le@gmail.com>
2020-09-23 18:04:36 -0500

    connect redux store to Login component and map state to props to access username and password initial state

--- src/components/navigation/Login.js
+++ src/components/navigation/Login.js
@@ -1,15 +1,24 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
-export default function Login() {
+function Login({username, password}) {
     return (
         <div>
             <h1>Login/SignUp</h1>
             <h4>This will be Login / Setup Account page feature.</h4>
             <form>
-                <input type='text' placeholder='username' />
-                <input type='password' placeholder='password' />
-                <input type='submit' placeholder='Login/Signup' />
+                <input value={username} onChange={undefined} type='text' placeholder='username' />
+                <input value={password} onChange={undefined} type='password' placeholder='password' />
+                <input value={undefined} onChange={undefined} type='submit' placeholder='Login/Signup' />
             </form>
         </div>
     )
 }
+
+const mapStateToProps = state => {
+    return {
+        username: state.username,
+        password: state.password
+    }
+}
+export default connect(mapStateToProps)(Login)
