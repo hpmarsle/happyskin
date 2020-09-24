1b43a13e7ceaaf1397657ab4f2ffe69e793a4441
Maria Le <hp.mars.le@gmail.com>
2020-09-24 12:12:01 -0500

    created getCurrentUser async action creator to check if anyone is logged in when app mounts

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -5,10 +5,34 @@ export const setCurrentUser = user => {
         user
     }
 }
+
 //asynchronous action creator
+export const getCurrentUser = () => {
+    return dispatch => {
+        return fetch("http://localhost:3000/api/v1/get_current_user", {
+            credentials: "include",
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify()
+        })
+        .then(response => response.json())
+        .then(user => {
+            if (user.error) {
+                alert(user.error)
+            } else {
+                dispatch(setCurrentUser(user))
+            }
+        })
+        .catch(console.log)
+    }
+}
+
 export const login = credentials => {
     return dispatch => {
         return fetch("http://localhost:3000/api/v1/login", {
+            credentials: "include",
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
