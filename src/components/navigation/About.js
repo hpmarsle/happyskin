8aa4a9229ae24de914d6025f327e79585b3ef284
Maria Le <hp.mars.le@gmail.com>
2020-09-27 14:55:57 -0500

    edit About Page to be about happyskin company philosophy

--- src/components/navigation/About.js
+++ src/components/navigation/About.js
@@ -3,11 +3,20 @@ import React from 'react'
 export default function About() {
     return (
         <div>
-            <h1>About Happy Skin</h1>
+            <h1>Our Philosophy</h1>
             <p>
-                Happy Skin is a place where you can only review products for now.
-                Future updates, this will be an app that will mock an ecommerce store where you can sign up for an account, login, and buy the products by adding them to a shopping cart.                
+                We have the highest standards for safe, clean, and non-sensitizing skincare. We stand by our products and services. If you’re worried about toxins, sensitizing agents, fragrance or adverse reactions, you can rest easy shopping with us. 
             </p>
+            <p>
+                We believe skin is bio-individual and not even "good" or "cult-favorite" products work for everyone. Our team consists of people who have all different skin types: normal, dry, aging, and sensitive chronic conditions like acne, eczema, rosacea, hyperpigmentation, and psoraisis. 
+                Wherever you are on your skincare journey, we’re here to provide a platform where you can shop clean products and read HONEST reviews from our customers. We know the importance of honest and transparent feedback. If a product doesn't work for you, please contact us for a full refund or we can find something else that may work for you. No questions asked. Whether you have perfect or problematic skin: We're here for you. We appreciate you. We love you. 
+            </p>
+            
+            <p>
+                Wishing you the best skin health!
+            </p>
+            <p>- The Happyskin Team</p>
+     
         </div>
     )
 }
