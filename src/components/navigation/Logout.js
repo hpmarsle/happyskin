656996e0cd6d7de235cedaceaad4bba5e9239665
Maria Le <hp.mars.le@gmail.com>
2020-09-27 02:27:57 -0500

    import withRouter from react-router dom, edit onsubmit event handler to prevent refresh and pass history as props to redirect to home route after logout

--- src/components/navigation/Logout.js
+++ src/components/navigation/Logout.js
@@ -1,17 +1,22 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { logout } from '../../actions/currentUser'
+import { withRouter } from 'react-router-dom'
 
-function Logout({ logout }) {
+function Logout({ logout, history }) {
 
     return (
         <div>
             <h1>Logout</h1>
-            <form onSubmit={logout}>
+            <form onSubmit={e =>{ 
+                e.preventDefault()
+                logout()
+                history.push("/")
+            }}>
                 <input type='submit' value='Logout' />
             </form>
         </div>
     )
 }
 
-export default connect(null, { logout })(Logout)
+export default withRouter(connect(null, { logout })(Logout))
