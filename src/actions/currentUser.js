667590f41df082e9f27e45f237ad1104831f2d63
Maria Le <hp.mars.le@gmail.com>
2020-11-01 17:23:10 -0600

    Update getCurrentUser logic to only set current user if there is one

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -1,66 +1,68 @@
-export const setCurrentUser = user => {
-    return {
-        type: "SET_CURRENT_USER", 
-        user
-    }
+export const setCurrentUser = (user) => {
+  return {
+    type: 'SET_CURRENT_USER',
+    user,
+  }
 }
 
 export const clearCurrentUser = () => {
-    return {
-        type: "CLEAR_CURRENT_USER"
-    }
+  return {
+    type: 'CLEAR_CURRENT_USER',
+  }
 }
 
 export const getCurrentUser = () => {
-    return dispatch => {
-        return fetch("http://localhost:3000/api/v1/get_current_user", {
-            credentials: "include",
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify()
-        })
-        .then(response => response.json())
-        .then(user => {
-            if (user.error) {
-                alert(user.error)
-            } else {
-                dispatch(setCurrentUser(user))
-            }
-        })
-    }
+  return (dispatch) => {
+    return fetch('http://localhost:3000/api/v1/get_current_user', {
+      credentials: 'include',
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(),
+    })
+      .then((response) => response.json())
+      .then((user) => {
+        if (user.error) {
+          alert(user.error)
+        } else if (user === '') {
+          return
+        } else {
+          dispatch(setCurrentUser(user))
+        }
+      })
+  }
 }
 
 export const login = (credentials, history) => {
-    return dispatch => {
-        return fetch("http://localhost:3000/api/v1/login", {
-            credentials: "include",
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(credentials)
-        })
-        .then(response => response.json())
-        .then(user => {
-            if (user.error) {
-                alert(user.error)
-            } else {
-                dispatch(setCurrentUser(user))
-                history.push("/")
-            }
-        })
-        .catch(console.log)
-    }
+  return (dispatch) => {
+    return fetch('http://localhost:3000/api/v1/login', {
+      credentials: 'include',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(credentials),
+    })
+      .then((response) => response.json())
+      .then((user) => {
+        if (user.error) {
+          alert(user.error)
+        } else {
+          dispatch(setCurrentUser(user))
+          history.push('/')
+        }
+      })
+      .catch(console.log)
+  }
 }
 
 export const logout = () => {
-    return (dispatch) => {
-        dispatch(clearCurrentUser())
-        return fetch("http://localhost:3000/api/v1/logout", {
-            credentials: "include",
-            method: "DELETE"
-        })
-    }  
+  return (dispatch) => {
+    dispatch(clearCurrentUser())
+    return fetch('http://localhost:3000/api/v1/logout', {
+      credentials: 'include',
+      method: 'DELETE',
+    })
+  }
 }
