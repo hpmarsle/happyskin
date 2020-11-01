8863c9595b62920cd6d0bf9a2706d1942c3ded40
Maria Le <hp.mars.le@gmail.com>
2020-10-31 22:46:26 -0500

    add star_rating component to display on product cards and pages

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,44 +1,54 @@
 import React from 'react'
 import styled from 'styled-components'
+import StarRatingComponent from 'react-star-rating-component'
 
 const StyledDiv = styled.div`
-    margin: 20px;
-    padding: 10px;
-    border: solid pink;
-    text-align: center;
-    display: inline-block;
-    height: auto;
-    width: auto;
-    min-height: 500px;
-    min-width: 350px;
-    max-width: 1000px;
-    max-height: 1000px;
-    justify-content: center;
-
+  margin: 20px;
+  padding: 10px;
+  border: solid pink;
+  text-align: center;
+  display: inline-block;
+  height: auto;
+  width: auto;
+  min-height: 500px;
+  min-width: 350px;
+  max-width: 1000px;
+  max-height: 1000px;
+  justify-content: center;
 `
 const Image = styled.img`
-    border: 1px solid #ddd;
-    border-radius: 4px;
-    padding: 5px;
-    width: auto;
-    height: auto;
-    min-height: 400px;
-    max-width: 400px;
-    max-height: 400px;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+  padding: 5px;
+  width: auto;
+  height: auto;
+  min-height: 400px;
+  max-width: 400px;
+  max-height: 400px;
 `
 
 const ProductCard = (props) => {
-    return (
-        <StyledDiv>
-            <div id={props.id} >  
-                <Image src={props.image} alt="product pic"/>
-                <div> {props.name} </div>
-                <div>{props.brand} </div>
-                <div>${props.price}0 </div>
-                <div>{props.reviews.length >= 1 ? props.reviews[0].star_rating : "No Reviews"} </div>
-            </div>
-        </StyledDiv>
-    )
+  return (
+    <StyledDiv>
+      <div id={props.id}>
+        <Image src={props.image} alt='product pic' />
+        <div> {props.name} </div>
+        <div>{props.brand} </div>
+        <div>${props.price}0 </div>
+        <div>
+          {props.reviews.length >= 1 ? (
+            <StarRatingComponent
+              name='rate1'
+              starCount={5}
+              value={props.reviews[0].star_rating}
+            />
+          ) : (
+            'No Reviews'
+          )}{' '}
+        </div>
+      </div>
+    </StyledDiv>
+  )
 }
 
 export default ProductCard
--- src/components/Review.js
+++ src/components/Review.js
@@ -2,35 +2,43 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { deleteReview } from '../actions/reviews'
 import { Link } from 'react-router-dom'
-
+import StarRatingComponent from 'react-star-rating-component'
 
 class Review extends React.Component {
+  render() {
+    const { id, star_rating, comments, productId, deleteReview } = this.props
 
-    render() {
-        const {id, star_rating, comments, productId, deleteReview} = this.props
+    return (
+      <div id={id}>
+        <div>
+          <div className='star_rating'>
+            {star_rating}
+            <StarRatingComponent
+              name='rate1'
+              starCount={5}
+              value={star_rating}
+            />
+          </div>
+        </div>
 
-        return (           
-            <div id={id}>
-                <div>
-                    <div className='star_rating'>{star_rating} </div>
-                </div>
-    
-                <div>
-                    <div className='comments'>{comments} </div>
-                </div>
-                {/*<button onClick={event =>  window.location.href=`/products/${productId}/reviews/${id}`}>Edit</button>*/}
-                <Link to={{
-                    pathname: `/products/${productId}/reviews/${id}`,
-                    state: { star_rating, comments }
-                }}>
-                    Edit
-                </Link>
-                <button onClick={() => deleteReview(id, productId)}>Delete</button>
-                <br /><br />
-            </div>
-        )
-    }
+        <div>
+          <div className='comments'>{comments} </div>
+        </div>
+        {/*<button onClick={event =>  window.location.href=`/products/${productId}/reviews/${id}`}>Edit</button>*/}
+        <Link
+          to={{
+            pathname: `/products/${productId}/reviews/${id}`,
+            state: { star_rating, comments },
+          }}
+        >
+          Edit
+        </Link>
+        <button onClick={() => deleteReview(id, productId)}>Delete</button>
+        <br />
+        <br />
+      </div>
+    )
+  }
 }
 
-export default connect(null, {deleteReview})(Review)
-
+export default connect(null, { deleteReview })(Review)
