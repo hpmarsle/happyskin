de969f2a2894bc5a68f0b73897bd0558d35053ee
Maria Le <hp.mars.le@gmail.com>
2020-11-01 17:52:26 -0600

    Change star rating input to select dropdown to restrict input type submission

--- src/components/ReviewInput.js
+++ src/components/ReviewInput.js
@@ -4,64 +4,73 @@ import { connect } from 'react-redux'
 import styled from 'styled-components'
 
 const StyledDiv = styled.div`
-    margin: 2em;
-    padding: 1em;
-    border: solid pink;
-    height: auto;
-    width: auto;
-
+  margin: 2em;
+  padding: 1em;
+  border: solid pink;
+  height: auto;
+  width: auto;
 `
 class ReviewInput extends React.Component {
-    state = {
+  state = {
+    star_rating: '',
+    comments: '',
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+
+    if (this.state.star_rating === '' || this.state.comments === '') {
+      alert('Please fill out both fields')
+    } else {
+      this.props.addReview(this.state, this.props.product.id)
+      this.setState({
         star_rating: '',
-        comments: ''
+        comments: '',
+      })
     }
+  }
 
-    handleChange = (event) => {
-        this.setState({
-            [event.target.name]: event.target.value
-        })
-    }
+  render() {
+    return (
+      <StyledDiv>
+        <h3>Leave a Review</h3>
+        <form onSubmit={this.handleSubmit}>
+          <select
+            id='star_rating'
+            name='star_rating'
+            value={this.state.star_rating}
+            onChange={this.handleChange}
+          >
+            <option selected value=''>
+              Please rate 1-5 stars.
+            </option>
+            <option value={1}>1</option>
+            <option value={2}>2</option>
+            <option value={3}>3</option>
+            <option value={4}>4</option>
+            <option value={5}>5</option>
+          </select>
 
-    handleSubmit = (event) => {
-        event.preventDefault()
-
-        if (this.state.star_rating === "" || this.state.comments === "") {
-            alert("Please fill out both fields");
-        } else {
-            this.props.addReview(this.state, this.props.product.id)
-            this.setState({
-                star_rating: '',
-                comments: ''
-            })
-        }
-
-    }
-    
-    render() {
-        return (
-            <StyledDiv>
-                <h3>Leave a Review</h3>
-                <form onSubmit={this.handleSubmit}>  
-                    <label for="star_rating">Rating</label>
-                    <input 
-                        type="text"
-                        name="star_rating" 
-                        value={this.state.star_rating} 
-                        onChange={this.handleChange} 
-                        placeholder="please type 1-5 stars"
-                    />
-                
-                    <label for="comments">Comments</label>
-                    <textarea type="text"name="comments" value={this.state.comments} onChange={this.handleChange}/>
-                    <div className="formButton">
-                        <input type="submit"/>
-                    </div>
-                </form>
-            </StyledDiv>
-        )
-    }
+          <label for='comments'>Comments</label>
+          <textarea
+            type='text'
+            name='comments'
+            value={this.state.comments}
+            onChange={this.handleChange}
+          />
+          <div className='formButton'>
+            <input type='submit' />
+          </div>
+        </form>
+      </StyledDiv>
+    )
+  }
 }
 
-
-export default connect(null, {addReview})(ReviewInput)
+export default connect(null, { addReview })(ReviewInput)
