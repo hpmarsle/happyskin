62db40a7b0c73a0f300d7c56ad7e5b0602b09f3a
Maria Le <hp.mars.le@gmail.com>
2020-11-05 15:22:21 -0600

    Style button with MaterialUI

--- src/components/navigation/Login.js
+++ src/components/navigation/Login.js
@@ -2,38 +2,52 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { updateLoginForm } from '../../actions/loginForm'
 import { login } from '../../actions/currentUser'
+import { Button } from '@material-ui/core'
 
-
-function Login({loginFormData, updateLoginForm, login, history }) {
-    const handleInputChange = event => {
-        const { name, value } = event.target
-        const updatedFormInfo = {
-            ...loginFormData,
-            [name]: value
-        }
-        updateLoginForm(updatedFormInfo)
+function Login({ loginFormData, updateLoginForm, login, history }) {
+  const handleInputChange = (event) => {
+    const { name, value } = event.target
+    const updatedFormInfo = {
+      ...loginFormData,
+      [name]: value,
     }
+    updateLoginForm(updatedFormInfo)
+  }
 
-    const handleSubmit = event => {
-        event.preventDefault()
-        login(loginFormData, history)
-    }
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    login(loginFormData, history)
+  }
 
-    return (
-        <div>
-            <h1>Login</h1>
-            <form onSubmit={handleSubmit}>
-                <input value={loginFormData.username} name="username" onChange={handleInputChange} type='text' placeholder='username' />
-                <input value={loginFormData.password} name="password" onChange={handleInputChange} type='password' placeholder='password' />
-                <input type='submit' value="Login" />
-            </form>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={loginFormData.username}
+          name='username'
+          onChange={handleInputChange}
+          type='text'
+          placeholder='username'
+        />
+        <input
+          value={loginFormData.password}
+          name='password'
+          onChange={handleInputChange}
+          type='password'
+          placeholder='password'
+        />
+        <Button variant='contained' color='primary' type='submit'>
+          Login
+        </Button>
+      </form>
+    </div>
+  )
 }
 
-const mapStateToProps = state => {
-    return {
-        loginFormData: state.loginForm
-    }
+const mapStateToProps = (state) => {
+  return {
+    loginFormData: state.loginForm,
+  }
 }
 export default connect(mapStateToProps, { updateLoginForm, login })(Login)
