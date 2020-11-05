060bb34460955a219e11504224afea54b95398d8
Maria Le <hp.mars.le@gmail.com>
2020-11-05 15:21:43 -0600

    Convert Review class component to functional component, Use MaterialUI for button. Edit form is not holding state of prior value anymore when button is clicked and redirects to edit form

--- src/components/Review.js
+++ src/components/Review.js
@@ -1,39 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { connect } from 'react-redux'
 import { deleteReview } from '../actions/reviews'
 import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Button } from '@material-ui/core'
 
-class Review extends React.Component {
-  render() {
-    const { id, star_rating, comments, productId, deleteReview } = this.props
+function Review({ id, star_rating, comments, productId, deleteReview }) {
+  // const [reviewStars, setStars] = useState(star_rating)
+  // const [reviewComments, setComments] = useState(comments)
 
-    return (
-      <div id={id}>
-        <div>
-          <div className='star_rating'>
-            {new Array(star_rating).fill(<FontAwesomeIcon icon='star' />)}
-          </div>
+  return (
+    <div id={id}>
+      <div>
+        <div className='star_rating'>
+          {new Array(star_rating).fill(<FontAwesomeIcon icon='star' />)}
         </div>
-
-        <div>
-          <div className='comments'>{comments} </div>
-        </div>
-        {/*<button onClick={event =>  window.location.href=`/products/${productId}/reviews/${id}`}>Edit</button>*/}
-        <Link
-          to={{
-            pathname: `/products/${productId}/reviews/${id}`,
-            state: { star_rating, comments },
-          }}
-        >
-          Edit
-        </Link>
-        <button onClick={() => deleteReview(id, productId)}>Delete</button>
-        <br />
-        <br />
       </div>
-    )
-  }
+
+      <div>
+        <div className='comments'>{comments} </div>
+      </div>
+      {/*<button onClick={event =>  window.location.href=`/products/${productId}/reviews/${id}`}>Edit</button>*/}
+
+      <Button
+        variant='outlined'
+        color='primary'
+        size='small'
+        onClick={(event) =>
+          (window.location.href = `/products/${productId}/reviews/${id}`)
+        }
+        star_rating={star_rating}
+        comments={comments}
+      >
+        Edit
+      </Button>
+      <Button
+        variant='outlined'
+        color='primary'
+        onClick={() => deleteReview(id, productId)}
+        size='small'
+      >
+        Delete
+      </Button>
+      <br />
+      <br />
+    </div>
+  )
 }
 
 export default connect(null, { deleteReview })(Review)
--- src/components/ReviewInputEdit.js
+++ src/components/ReviewInputEdit.js
@@ -2,48 +2,54 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { editReview } from '../actions/reviews'
 
-class ReviewInputEdit extends React.Component{
-    constructor(props){
-        super(props)
-        this.state = {
-            star_rating: props.location.state.star_rating,
-            comments: props.location.state.comments,
-            id: props.match.params.reviewId,
-            product_id: props.match.params.productId
-        }
+class ReviewInputEdit extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      star_rating: props.star_rating,
+      comments: props.comments,
+      id: props.match.params.reviewId,
+      product_id: props.match.params.productId,
     }
+  }
 
-    handleChange = (event) => {
-        this.setState({
-            [event.target.name]: event.target.value
-        })
-    }
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    })
+  }
 
-    handleSubmit = (event) => {
-        this.props.editReview(this.state.id, this.state.product_id, this.state)
-        this.setState({
-            star_rating: '',
-            comments: ''
-        })
-        this.props.history.push(`/products/${this.state.product_id}`)
-
-    }
-    render() {
-
-        return (
-            <div>
-                <h3>Edit Review</h3>
-                <form onSubmit={this.handleSubmit}>
-                    <label for="star_rating">Rating</label>
-                    <input type="text"name="star_rating" value={this.state.star_rating} onChange={this.handleChange}/>
-                    <label for="comments">Comments:</label>
-                    <textarea name="comments" value={this.state.comments} onChange={this.handleChange}/>
-                    <input type="submit"/>
-                </form>
-            </div>
-        )
-    }
+  handleSubmit = (event) => {
+    this.props.editReview(this.state.id, this.state.product_id, this.state)
+    this.setState({
+      star_rating: '',
+      comments: '',
+    })
+    this.props.history.push(`/products/${this.state.product_id}`)
+  }
+  render() {
+    return (
+      <div>
+        <h3>Edit Review</h3>
+        <form onSubmit={this.handleSubmit}>
+          <label for='star_rating'>Rating</label>
+          <input
+            type='text'
+            name='star_rating'
+            value={this.state.star_rating}
+            onChange={this.handleChange}
+          />
+          <label for='comments'>Comments:</label>
+          <textarea
+            name='comments'
+            value={this.state.comments}
+            onChange={this.handleChange}
+          />
+          <input type='submit' />
+        </form>
+      </div>
+    )
+  }
 }
-
 
 export default connect(null, { editReview })(ReviewInputEdit)
