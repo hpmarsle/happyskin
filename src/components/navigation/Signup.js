81dee2f0094b5421edf45ee44a8a22907b3a670a
Maria Le <hp.mars.le@gmail.com>
2020-11-23 23:34:35 -0600

    connect Signup component to Redux store

--- src/components/navigation/Signup.js
+++ src/components/navigation/Signup.js
@@ -1,21 +1,66 @@
 import React from 'react'
+import { updateSignupForm } from '../../actions/signupForm'
+import { signup } from '../../actions/currentUser'
 
-export default function Signup() {
+import { connect } from 'react-redux'
+
+function Signup({ signupFormData, updateSignupForm, signup, history }) {
+  const handleInputChange = (event) => {
+    const { name, value } = event.target
+    const updatedFormInfo = {
+      ...signupFormData,
+      [name]: value,
+    }
+    updateSignupForm(updatedFormInfo)
+  }
+
   const handleSubmit = (event) => {
     event.preventDefault()
-
-    //send form and create new user with it, send to backend
+    signup(signupFormData, history)
   }
+
   return (
     <div>
       <h1>Signup</h1>
       <form onSubmit={handleSubmit}>
-        <input name='name' type='text' placeholder='name' />
-        <input name='email' type='email' placeholder='email' />
-        <input name='username' type='text' placeholder='username' />
-        <input name='password' type='password' placeholder='password' />
+        <input
+          value={signupFormData.name}
+          onChange={handleInputChange}
+          name='name'
+          type='text'
+          placeholder='name'
+        />
+        <input
+          value={signupFormData.email}
+          onChange={handleInputChange}
+          name='email'
+          type='email'
+          placeholder='email'
+        />
+        <input
+          value={signupFormData.username}
+          onChange={handleInputChange}
+          name='username'
+          type='text'
+          placeholder='username'
+        />
+        <input
+          value={signupFormData.password}
+          onChange={handleInputChange}
+          name='password'
+          type='password'
+          placeholder='password'
+        />
         <input type='submit' value='Signup' />
       </form>
     </div>
   )
 }
+
+const mapStateToProps = (state) => {
+  return {
+    signupFormData: state.signupForm,
+  }
+}
+
+export default connect(mapStateToProps, { updateSignupForm, signup })(Signup)
